e2c9d38d98f65ea6458aeb9df1b7ec655a315fa7
khanh vinh nguyen <khanhvinh.nguyen.0628@gmail.com>
2021-09-29 18:09:34 +0700

    issue-21: add to home screen

--- src/components/InApp/check-browser.js
+++ src/components/InApp/check-browser.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import InApp from './inapp';
+
+import AddToHomeScreen from './AddToHomescreen/addToHomescreen'
 // import WelcomeScreen from './Slider/Welcome-screen';
 
 class CheckBrowser extends Component {
@@ -15,24 +17,24 @@ class CheckBrowser extends Component {
   }
 
   render() {
-    const slides = [
-      {
-          key: 1,
-          title: 'Welcome To SendLotus',
-          des: 'To experience sendlotus the most optimal way',
-          image: '',
-      },
-      {
-          key: 2,
-          title: 'First click on the Share button in the red rectangle as below:',
-          image: '',
-      },
-      {
-          key: 3,
-          title: '',
-          image: '',
-      },
-    ];
+    // const slides = [
+    //   {
+    //       key: 1,
+    //       title: 'Welcome To SendLotus',
+    //       des: 'To experience sendlotus the most optimal way',
+    //       image: '',
+    //   },
+    //   {
+    //       key: 2,
+    //       title: 'First click on the Share button in the red rectangle as below:',
+    //       image: '',
+    //   },
+    //   {
+    //       key: 3,
+    //       title: '',
+    //       image: '',
+    //   },
+    // ];
 
     const { inapp } = this.state;
 
@@ -44,14 +46,52 @@ class CheckBrowser extends Component {
 
     else if (inapp.isMobile) // isMobile
     {
-      return alert('For the best user experience, please install app');
+      return (
+        <div>
+        <AddToHomeScreen
+            startAutomatically={ true }
+            startDelay={ 0 }
+            lifespan={ 30 }
+            skipFirstVisit={ true }
+            displayPace={ 0 }
+            customPromptContent={ {
+              cancelMsg: '',
+              installMsg: 'Install',
+              guidanceCancelMsg: ''
+            } }
+            customPromptElements={ {
+              container: 'athContainer',
+              containerAddOns: '',
+              banner: 'athBanner',
+              logoCell: 'athLogoCell',
+              logoCellAddOns: 'athContentCell',
+              logo: 'athLogo',
+              titleCell: 'athTitleCell',
+              titleCellAddOns: 'athContentCell',
+              title: 'athTitle',
+              cancelButtonCell: 'athCancelButtonCell',
+              cancelButtonCellAddOns: 'athButtonCell',
+              cancelButton: 'athCancelButton',
+              installButtonCell: 'athInstallButtonCell',
+              installButtonCellAddOns: 'athButtonCell',
+              installButton: 'athInstallButton',
+              installButtonAddOns: 'button',
+              guidance: 'athGuidance',
+              guidanceImageCell: 'athGuidanceImageCell',
+              guidanceImageCellAddOns: '',
+              guidanceCancelButton: 'athGuidanceCancelButton'
+            } }
+        />
+        </div>
+      );
+      // return <AddToHomescreen/>;
       // return <WelcomeScreen slides={slides}/>;
     } 
     
 
     else // isInApp
     {
-      return null;
+      return alert('For the best user experience, please install app');
     }
   }
 }
