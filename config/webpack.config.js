3f98317c5da4730f522f6c8cb668da4907daeaeb
vince8x <vince.tran8x@gmail.com>
2021-10-08 17:01:15 +0700

    Fix config show styled components name

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -482,6 +482,7 @@ module.exports = function (webpackEnv) {
                                             },
                                         },
                                     ],
+                                    "babel-plugin-styled-components"
                                 ],
                                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                                 // It enables caching results in ./node_modules/.cache/babel-loader/
