f1a46d5f1ebe91e94e52e802f12a099849434af2
tunghoang <thtung2901@gmail.com>
2022-01-12 12:06:50 +0700

    format code

--- src/components/Common/EnhancedInputs.js
+++ src/components/Common/EnhancedInputs.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { Form, FormItemProps, Input, Select } from 'antd';
 import {
-    ThemedDollarOutlined,
-    ThemedWalletOutlined,
+	ThemedDollarOutlined,
+	ThemedWalletOutlined,
 } from '@components/Common/CustomIcons';
 import { LockOutlined } from '@ant-design/icons';
 import styled, { css } from 'styled-components';
@@ -15,14 +15,14 @@ import { currency } from '@components/Common/Ticker.js';
 export const AntdFormCss = css`
     .ant-input-group-addon {
         background-color: ${props =>
-        props.theme.forms.addonBackground} !important;
+		props.theme.forms.addonBackground} !important;
         border: 1px solid ${props => props.theme.forms.border};
         color: ${props => props.theme.forms.addonForeground} !important;
     }
     input.ant-input,
     .ant-select-selection {
         background-color: ${props =>
-        props.theme.forms.selectionBackground} !important;
+		props.theme.forms.selectionBackground} !important;
         box-shadow: none !important;
         border-radius: 4px;
         font-weight: bold;
@@ -89,11 +89,11 @@ export const InputAddonText = styled.span`
     display: block;
 
     ${props =>
-        props.disabled
-            ? `
+		props.disabled
+			? `
       cursor: not-allowed;
       `
-            : `cursor: pointer;`}
+			: `cursor: pointer;`}
 `;
 
 export const InputNumberAddonText = styled.span`
@@ -107,168 +107,168 @@ export const InputNumberAddonText = styled.span`
         color: ${props => props.theme.forms.addonForeground} !important;
     }
     ${props =>
-        props.disabled
-            ? `
+		props.disabled
+			? `
       cursor: not-allowed;
       `
-            : `cursor: pointer;`}
+			: `cursor: pointer;`}
 `;
 
 export const SendBchInput = ({
-    onMax,
-    inputProps,
-    selectProps,
-    activeFiatCode,
-    ...otherProps
+	onMax,
+	inputProps,
+	selectProps,
+	activeFiatCode,
+	...otherProps
 }) => {
-    const { Option } = Select;
-    const currencies = [
-        {
-            value: currency.ticker,
-            label: currency.ticker,
-        }
-        // {
-        //     value: activeFiatCode ? activeFiatCode : 'USD',
-        //     label: activeFiatCode ? activeFiatCode : 'USD',
-        // },
-    ];
-    const currencyOptions = currencies.map(currency => {
-        return (
-            <Option
-                key={currency.value}
-                value={currency.value}
-                className="selectedCurrencyOption"
-            >
-                {currency.label}
-            </Option>
-        );
-    });
+	const { Option } = Select;
+	const currencies = [
+		{
+			value: currency.ticker,
+			label: currency.ticker,
+		}
+		// {
+		//     value: activeFiatCode ? activeFiatCode : 'USD',
+		//     label: activeFiatCode ? activeFiatCode : 'USD',
+		// },
+	];
+	const currencyOptions = currencies.map(currency => {
+		return (
+			<Option
+				key={currency.value}
+				value={currency.value}
+				className="selectedCurrencyOption"
+			>
+				{currency.label}
+			</Option>
+		);
+	});
 
-    const CurrencySelect = (
-        <Select
-            defaultValue={currency.ticker}
-            className="select-after"
-            style={{ width: '25%' }}
-            {...selectProps}
-        >
-            {currencyOptions}
-        </Select>
-    );
-    return (
-        <AntdFormWrapper>
-            <Form.Item {...otherProps}>
-                <Input.Group compact>
-                    <Input
-                        style={{ width: '58%', textAlign: 'left' }}
-                        type="number"
-                        step={
-                            inputProps.dollar === 1
-                                ? 0.01
-                                : 1 / 10 ** currency.cashDecimals
-                        }
-                        prefix={
-                            inputProps.dollar === 1 ? (
-                                <ThemedDollarOutlined />
-                            ) : (
-                                <img
-                                    src={currency.logo}
-                                    alt=""
-                                    width={16}
-                                    height={16}
-                                />
-                            )
-                        }
-                        {...inputProps}
-                    />
-                    {CurrencySelect}
-                    <InputNumberAddonText
-                        style={{
-                            width: '17%',
-                            height: '60px',
-                            lineHeight: '60px',
-                        }}
-                        disabled={!!(inputProps || {}).disabled}
-                        onClick={!(inputProps || {}).disabled && onMax}
-                    >
-                        max
-                    </InputNumberAddonText>
-                </Input.Group>
-            </Form.Item>
-        </AntdFormWrapper>
-    );
+	const CurrencySelect = (
+		<Select
+			defaultValue={currency.ticker}
+			className="select-after"
+			style={{ width: '25%' }}
+			{...selectProps}
+		>
+			{currencyOptions}
+		</Select>
+	);
+	return (
+		<AntdFormWrapper>
+			<Form.Item {...otherProps}>
+				<Input.Group compact>
+					<Input
+						style={{ width: '58%', textAlign: 'left' }}
+						type="number"
+						step={
+							inputProps.dollar === 1
+								? 0.01
+								: 1 / 10 ** currency.cashDecimals
+						}
+						prefix={
+							inputProps.dollar === 1 ? (
+								<ThemedDollarOutlined />
+							) : (
+								<img
+									src={currency.logo}
+									alt=""
+									width={16}
+									height={16}
+								/>
+							)
+						}
+						{...inputProps}
+					/>
+					{CurrencySelect}
+					<InputNumberAddonText
+						style={{
+							width: '17%',
+							height: '60px',
+							lineHeight: '60px',
+						}}
+						disabled={!!(inputProps || {}).disabled}
+						onClick={!(inputProps || {}).disabled && onMax}
+					>
+						max
+					</InputNumberAddonText>
+				</Input.Group>
+			</Form.Item>
+		</AntdFormWrapper>
+	);
 };
 
 SendBchInput.propTypes = {
-    onMax: PropTypes.func,
-    inputProps: PropTypes.object,
-    selectProps: PropTypes.object,
-    activeFiatCode: PropTypes.string,
+	onMax: PropTypes.func,
+	inputProps: PropTypes.object,
+	selectProps: PropTypes.object,
+	activeFiatCode: PropTypes.string,
 };
 
 export const FormItemWithMaxAddon = ({ onMax, inputProps, ...otherProps }) => {
-    return (
-        <AntdFormWrapper>
-            <Form.Item {...otherProps}>
-                <Input
-                    type="number"
-                    prefix={
-                        <img
-                            src={currency.logo}
-                            alt=""
-                            width={16}
-                            height={16}
-                        />
-                    }
-                    addonAfter={
-                        <InputAddonText
-                            disabled={!!(inputProps || {}).disabled}
-                            onClick={!(inputProps || {}).disabled && onMax}
-                        >
-                            max
-                        </InputAddonText>
-                    }
-                    {...inputProps}
-                />
-            </Form.Item>
-        </AntdFormWrapper>
-    );
+	return (
+		<AntdFormWrapper>
+			<Form.Item {...otherProps}>
+				<Input
+					type="number"
+					prefix={
+						<img
+							src={currency.logo}
+							alt=""
+							width={16}
+							height={16}
+						/>
+					}
+					addonAfter={
+						<InputAddonText
+							disabled={!!(inputProps || {}).disabled}
+							onClick={!(inputProps || {}).disabled && onMax}
+						>
+							max
+						</InputAddonText>
+					}
+					{...inputProps}
+				/>
+			</Form.Item>
+		</AntdFormWrapper>
+	);
 };
 
 FormItemWithMaxAddon.propTypes = {
-    onMax: PropTypes.func,
-    inputProps: PropTypes.object,
+	onMax: PropTypes.func,
+	inputProps: PropTypes.object,
 };
 
 // loadWithCameraOpen prop: if true, load page with camera scanning open
 export const FormItemWithQRCodeAddon = ({
-    onScan,
-    loadWithCameraOpen,
-    inputProps,
-    ...otherProps
+	onScan,
+	loadWithCameraOpen,
+	inputProps,
+	...otherProps
 }) => {
-    return (
-        <AntdFormWrapper>
-            <Form.Item {...otherProps}>
-                <Input
-                    prefix={<ThemedWalletOutlined />}
-                    autoComplete="off"
-                    addonAfter={
-                        <ScanQRCode
-                            loadWithCameraOpen={loadWithCameraOpen}
-                            onScan={onScan}
-                        />
-                    }
-                    {...inputProps}
-                />
-            </Form.Item>
-        </AntdFormWrapper>
-    );
+	return (
+		<AntdFormWrapper>
+			<Form.Item {...otherProps}>
+				<Input
+					prefix={<ThemedWalletOutlined />}
+					autoComplete="off"
+					addonAfter={
+						<ScanQRCode
+							loadWithCameraOpen={loadWithCameraOpen}
+							onScan={onScan}
+						/>
+					}
+					{...inputProps}
+				/>
+			</Form.Item>
+		</AntdFormWrapper>
+	);
 };
 
 FormItemWithQRCodeAddon.propTypes = {
-    onScan: PropTypes.func,
-    loadWithCameraOpen: PropTypes.bool,
-    inputProps: PropTypes.object,
+	onScan: PropTypes.func,
+	loadWithCameraOpen: PropTypes.bool,
+	inputProps: PropTypes.object,
 };
 
 // loadWithCameraOpen prop: if true, load page with camera scanning open
@@ -279,21 +279,21 @@ export const FormItemWithScanRedeemQRCodeAddon = ({
 	...otherProps
 }) => {
 	return (
-			<AntdFormWrapper>
-					<Form.Item {...otherProps}>
-							<Input
-									prefix={<LockOutlined />}
-									autoComplete="off"
-									addonAfter={
-											<ScanRedeemQRCode
-													loadWithCameraOpen={loadWithCameraOpen}
-													onScan={onScan}
-											/>
-									}
-									{...inputProps}
-							/>
-					</Form.Item>
-			</AntdFormWrapper>
+		<AntdFormWrapper>
+			<Form.Item {...otherProps}>
+				<Input
+					prefix={<LockOutlined />}
+					autoComplete="off"
+					addonAfter={
+						<ScanRedeemQRCode
+							loadWithCameraOpen={loadWithCameraOpen}
+							onScan={onScan}
+						/>
+					}
+					{...inputProps}
+				/>
+			</Form.Item>
+		</AntdFormWrapper>
 	);
 };
 
@@ -304,62 +304,62 @@ FormItemWithScanRedeemQRCodeAddon.propTypes = {
 };
 
 export const CurrencySelectDropdown = selectProps => {
-    const { Option } = Select;
+	const { Option } = Select;
 
-    // Build select dropdown from currency.fiatCurrencies
-    const currencyMenuOptions = [];
-    const currencyKeys = Object.keys(currency.fiatCurrencies);
-    for (let i = 0; i < currencyKeys.length; i += 1) {
-        const currencyMenuOption = {};
-        currencyMenuOption.value =
-            currency.fiatCurrencies[currencyKeys[i]].slug;
-        currencyMenuOption.label = `${currency.fiatCurrencies[currencyKeys[i]].name
-            } (${currency.fiatCurrencies[currencyKeys[i]].symbol})`;
-        currencyMenuOptions.push(currencyMenuOption);
-    }
-    const currencyOptions = currencyMenuOptions.map(currencyMenuOption => {
-        return (
-            <Option
-                key={currencyMenuOption.value}
-                value={currencyMenuOption.value}
-                className="selectedCurrencyOption"
-            >
-                {currencyMenuOption.label}
-            </Option>
-        );
-    });
+	// Build select dropdown from currency.fiatCurrencies
+	const currencyMenuOptions = [];
+	const currencyKeys = Object.keys(currency.fiatCurrencies);
+	for (let i = 0; i < currencyKeys.length; i += 1) {
+		const currencyMenuOption = {};
+		currencyMenuOption.value =
+			currency.fiatCurrencies[currencyKeys[i]].slug;
+		currencyMenuOption.label = `${currency.fiatCurrencies[currencyKeys[i]].name
+			} (${currency.fiatCurrencies[currencyKeys[i]].symbol})`;
+		currencyMenuOptions.push(currencyMenuOption);
+	}
+	const currencyOptions = currencyMenuOptions.map(currencyMenuOption => {
+		return (
+			<Option
+				key={currencyMenuOption.value}
+				value={currencyMenuOption.value}
+				className="selectedCurrencyOption"
+			>
+				{currencyMenuOption.label}
+			</Option>
+		);
+	});
 
-    return (
-        <Select
-            className="select-after"
-            style={{
-                width: '100%',
-            }}
-            {...selectProps}
-        >
-            {currencyOptions}
-        </Select>
-    );
+	return (
+		<Select
+			className="select-after"
+			style={{
+				width: '100%',
+			}}
+			{...selectProps}
+		>
+			{currencyOptions}
+		</Select>
+	);
 };
 
 export const AddressValidators = () => {
-    const { BCH } = useBCH();
+	const { BCH } = useBCH();
 
-    return {
-        safelyDetectAddressFormat: value => {
-            try {
-                return BCH.Address.detectAddressFormat(value);
-            } catch (error) {
-                return null;
-            }
-        },
-        isSLPAddress: value =>
-            AddressValidators.safelyDetectAddressFormat(value) === 'slpaddr',
-        isBCHAddress: value =>
-            AddressValidators.safelyDetectAddressFormat(value) === 'cashaddr',
-        isLegacyAddress: value =>
-            AddressValidators.safelyDetectAddressFormat(value) === 'legacy',
-        isXAddress: value =>
-            AddressValidators.safelyDetectAddressFormat(value) === 'xaddr',
-    }();
+	return {
+		safelyDetectAddressFormat: value => {
+			try {
+				return BCH.Address.detectAddressFormat(value);
+			} catch (error) {
+				return null;
+			}
+		},
+		isSLPAddress: value =>
+			AddressValidators.safelyDetectAddressFormat(value) === 'slpaddr',
+		isBCHAddress: value =>
+			AddressValidators.safelyDetectAddressFormat(value) === 'cashaddr',
+		isLegacyAddress: value =>
+			AddressValidators.safelyDetectAddressFormat(value) === 'legacy',
+		isXAddress: value =>
+			AddressValidators.safelyDetectAddressFormat(value) === 'xaddr',
+	}();
 };
