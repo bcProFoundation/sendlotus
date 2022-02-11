7a1d62a51105258bf78d1652b3e43e57512bb93a
doantrung1197 <doantrung1197@gmail.com>
2022-02-11 22:17:24 +0700

    - Refactor code ater reivewed

--- src/components/Common/UploadImageToScan.js
+++ src/components/Common/UploadImageToScan.js
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
-import {Alert, Modal, Button, Upload} from 'antd';
+import React, { useState } from 'react';
+import { Upload, message } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
-import {BrowserQRCodeReader} from '@zxing/library';
-import {currency, isValidCashPrefix, isValidTokenPrefix} from '@components/Common/Ticker.js';
-import {Event} from '@utils/GoogleAnalytics';
+import { BrowserQRCodeReader } from '@zxing/library';
+import { currency, isValidCashPrefix, isValidTokenPrefix } from '@components/Common/Ticker.js';
+import { Event } from '@utils/GoogleAnalytics';
 
-export const StyledUploadOutlined = styled(UploadOutlined)`
+const StyledUploadOutlined = styled(UploadOutlined)`
   color: #6f2dbd !important;
   font-size: 20px;
 `
 
-export const StyledSpan = styled.span`
+const StyledSpan = styled.span`
     width: 50%;
     font-size: 15px;
     display: inline-flex;
@@ -42,7 +42,7 @@ const UploadImageToScan = ({
     beforeUpload: file => {
       const reader = new FileReader();
 
-      reader.onload = async(e) => {
+      reader.onload = async (e) => {
         const imageUrl = e.target.result;
         scanQrFromImage(imageUrl);
       };
@@ -50,15 +50,15 @@ const UploadImageToScan = ({
 
       // Prevent upload
       return false;
-    },
+    }
   };
 
   const teardownCodeReader = codeReader => {
     if (codeReader !== null) {
-        codeReader.reset();
-        codeReader.stop();
-        codeReader = null;
-        setActiveCodeReader(codeReader);
+      codeReader.reset();
+      codeReader.stop();
+      codeReader = null;
+      setActiveCodeReader(codeReader);
     }
   };
 
@@ -69,62 +69,58 @@ const UploadImageToScan = ({
 
     // If what scanner reads from QR code begins with 'bitcoincash:' or 'simpleledger:' or their successor prefixes
     if (isValidCashPrefix(content) || isValidTokenPrefix(content)) {
-        type = 'address';
-        values = { result: content };
-        // Event("Category", "Action", "Label")
-        // Track number of successful QR code scans
-        // BCH or slp?
-        let eventLabel = currency.ticker;
-        const isToken = content.split(currency.tokenPrefix).length > 1;
-        if (isToken) {
-            eventLabel = currency.tokenTicker;
-        }
-        Event('ScanQRCode.js', 'Address Scanned', eventLabel);
+      type = 'address';
+      values = {
+        result: content
+      };
+      let eventLabel = currency.ticker;
+      const isToken = content.split(currency.tokenPrefix).length > 1;
+      if (isToken) {
+        eventLabel = currency.tokenTicker;
+      }
+      Event('ScanQRCode.js', 'Address Scanned', eventLabel);
     }
     return { type, values };
   };
 
-  // Have to apply logic parse redeem code 
+  // Have to apply logic parse redeem code
   const parseRedeemContent = content => {
     let type = 'redeemCode';
-    let values = { result: content };
+    let values = {
+      result: content
+    };
     return { type, values };
   };
 
   const scanQrFromImage = async (imageUrl) => {
     const codeReader = new BrowserQRCodeReader();
     setActiveCodeReader(codeReader);
-    
+
     try {
-      const content = await codeReader.decodeFromImage(
-        undefined,
-        imageUrl
-      );
+      const content = await codeReader.decodeFromImage(undefined, imageUrl);
       let result = null;
 
       switch (codeType) {
-        case 'address':
-          result = parseAddressContent(content.text);
+        case 'address': result = parseAddressContent(content.text);
           break;
-        case 'redeemCode':
-            result = parseRedeemContent(content.text);
-            break;
+        case 'redeemCode': result = parseRedeemContent(content.text);
+          break;
 
         default:
-            break;
+          break;
       }
 
       // stop scanning and fill form if it's an address
-      if (result?.type === codeType) {
-          // Hide the scanner
-          onScan(result.values?.result);
-          return teardownCodeReader(codeReader);
+      if (result?.type === codeType) { // Hide the scanner
+        onScan(result.values?.result);
+        return teardownCodeReader(codeReader);
       }
     } catch (err) {
       console.log(`Error in QR scanner:`);
       console.log(err);
       console.log(JSON.stringify(err.message));
-      //setMobileErrorMsg(JSON.stringify(err.message));
+      // setMobileErrorMsg(JSON.stringify(err.message));
+      message.error(`Couldn't scan QR code from uploaded image`);
       setError(err);
       teardownCodeReader(codeReader);
     }
